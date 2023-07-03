906154a47f652e1b8edb995bf633c3896dad5b92
ayushreegit <136914118+ayushreegit@users.noreply.github.com>
2023-07-03 18:45:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+// Initialize Express app and other required configurations
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://ashokgehlot:33cMmsiAW4A5vYYZ@cluster1.riwdhug.mongodb.net/?retryWrites=true', {
+dbName:"Ayshree",
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.log('MongoDB connection error:', error));
+
+// Create User schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  usertype:{
+    type:String,
+    enum:["Agent","Client"],
+    default:"Client"
+  },
+  files: [
+    {
+      filename: String,
+      cloudinaryId: String,
+      url: String
+    }
+  ]
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Configure Multer for file upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage });
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: 'dxuaxmayo',
+  api_key: '955981974535128',
+  api_secret: 'TPrOqCY-6C_KmQplW_0NrnhYJ1A'
+});
+
+// User Registration API
+app.post('/register', async (req, res) => {
+  const { name, email, password ,usertype} = req.body;
+
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Create a new user
+    const user = new User({ name, email, password ,usertype});
+    await user.save();
+
+    res.status(201).json({ message: `${user.name} Registerd successfully`,user});
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// User Login API
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user based on the email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Check if the password matches
+    if (user.password !== password) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Set user session or generate JWT token for authentication
+
+    res.status(200).json({ message: `${user.name} logged in successfully` });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// User Logout API
+app.post('/logout', async (req, res) => {
+  // Clear user session or invalidate JWT token
+
+  res.status(200).json({ message: 'User logged out successfully' });
+});
+
+// File Upload API
+app.post('/upload/:userId', upload.array('files'), async (req, res) => {
+    const { files } = req;
+    const { userId } = req.params;
+  
+    try {
+      // Upload files to Cloudinary
+      const uploadPromises = files.map(file => cloudinary.uploader.upload(file.path));
+      const results = await Promise.all(uploadPromises);
+  
+      // Save the uploaded files information to the user's files array in the database
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const uploadedFiles = results.map(result => ({
+        filename: result.original_filename,
+        cloudinaryId: result.public_id,
+        url: result.secure_url
+      }));
+  
+      user.files.push(...uploadedFiles);
+  
+      await user.save();
+  
+      // Remove the temporary files
+      files.forEach(file => fs.unlinkSync(file.path));
+  
+      res.status(201).json({
+        message: 'Files uploaded successfully',
+        files: uploadedFiles
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+  
+
+app.get("/users",async(req,res)=>{
+    const users =await User.find({});
+    res.status(200).json({
+        success:true,
+        users
+    })
+
+})
+
+app.get("/user/:id",async(req,res)=>{
+    const user =await User.findById(req.params.id);
+    res.status(200).json({
+        success:true,
+        user
+    })
+
+})
+
+// Start the server
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
---
+++ userschema.js
@@ -0,0 +1,31 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   usertype: {
+//     type: String,
+//     enum: ["Agent", "Client"],
+//     required: true
+//   },
+//   documents: [
+//     {
+//       filename: String,
+//       cloudinaryId: String,
+//       url: String
+//     }
+//   ]
+// });
+
+// export const User = mongoose.model("User", userSchema);
